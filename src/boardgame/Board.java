ca3bdc373d89c7aeb0229e2b142fa2168549d4ee
Hugo Andreassa <hugo,andreassa@gmail.com>
2020-05-18 17:29:34 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,8 @@
 package boardgame;
 
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 public class Board {
 
 	private int rows;
@@ -48,6 +51,22 @@ public class Board {
 		
 		this.pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
+		if(piece(position) == null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
